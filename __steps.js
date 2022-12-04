09f16bd50341bc3e4579146407bd109ba35dc0d9
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-12-04 13:05:43 +0600

    first commit

---
+++ __steps.js
@@ -0,0 +1,25 @@
+/* 
+POST: 
+1. create post API
+2. use fetch with method POST
+3. add headers to the fetch: 'content-type': 'application/json'.
+4. add body to the fetch to send data.
+5. Make sure to use JSON.stringify to send data
+6. use express.json() as middleware 
+*/
+
+
+/* 
+Query:
+https://www.youtube.com/watch?v=a85F7cvw9XM
+path?name = value&name=value&name=
+*/
+
+/*
+mongoDb: 
+1. sign up mongodb atlas
+2. create database user 
+3. Whitelist your ip address
+4. database> connect > show entire code
+5. npm i mongodb
+*/
---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Simple Node Server Running');
+})
+
+app.use(cors())
+app.use(express.json())
+
+const users = [
+    { id: 1, name: 'Asif', email: 'asif@gmail.com' },
+    { id: 2, name: 'Rohan', email: 'rohan@gmail.com' },
+    { id: 3, name: 'Naim', email: 'naim@gmail.com' },
+];
+
+// username: dbuser1
+// password: syKHR8Pufw9MgqVM
+
+
+const uri = "mongodb+srv://dbuser1:syKHR8Pufw9MgqVM@cluster0.eqk1vsl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db('simpleNode').collection('users')
+        // const user = { name: 'Salam', email: 'Salam@gmailcom' }
+        // const result = await userCollection.insertOne(user);
+        // console.log(result);
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find({})
+            const users = await cursor.toArray()
+            res.send(users)
+        })
+
+        app.post('/users', async (req, res) => {
+            console.log('Post API called');
+            const user = req.body;
+            const result = await userCollection.insertOne(user)
+            console.log(result);
+            user._id = result.insertedId
+            res.send(user);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch((error) => console.log(error))
+
+// app.get('/users', (req, res) => {
+//     if (req.query.name) {
+//         const search = req.query.name;
+//         const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >= 0)
+//         res.send(filtered)
+//     }
+//     else {
+//         res.send(users)
+//     }
+// });
+
+// app.post('/users', (req, res) => {
+//     console.log('Post API called');
+//     const user = req.body;
+//     user.id = users.length + 1;
+//     users.push(user);
+//     console.log(user);
+//     res.send(user);
+// })
+
+app.listen(port, () => {
+    console.log(`Simple server running on port ${port}`);
+})
